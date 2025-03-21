3fc63afb64dc939ce88d6d8669041b8e98e254ac
Panagiotis Kourdis <panagiotis.kourdis@intel.com>
2025-03-21 08:00:54 +0000

    Log results based on env

--- intel/run_train.sh
+++ intel/run_train.sh
@@ -19,38 +19,41 @@ SYSTEM=${SYSTEM:-aurora}
 
 # Do check for valid PyTorch config
 
-ENV_TO_LOAD=./envs/${SYSTEM}/load_latest_env.sh
-source ${ENV_TO_LOAD} ${PT_CONFIG}
+ENV_TO_LOAD=${ENV:-latest_env}
+ENV_REL_PATH=./envs/${SYSTEM}/${ENV_TO_LOAD}.sh
+source ${ENV_REL_PATH} ${PT_CONFIG}
+ENV_FULL_PATH=$(realpath ${ENV_REL_PATH})
+ENV_NAME=$(basename $(realpath ${ENV_FULL_PATH}) .env)
 
-#if [[ "$SYSTEM" == "aurora" ]]; then
-#    IFS='.' read -ra ADDR <<< "`cat $PBS_NODEFILE | head -1`"
-#    export MASTER_ADDR=$ADDR".hsn.cm.aurora.alcf.anl.gov"
-#elif [[ "$SYSTEM" == "borealis" ]]; then
-#    export MASTER_ADDR=$(head -n 1 ${PBS_NODEFILE})
-#else
-#    echo "Uknown system ${SYSTEM}!"
-#    exit 1
-#fi
-export MASTER_ADDR=$(head -n 1 ${PBS_NODEFILE})
+if [[ "$SYSTEM" == "aurora" ]]; then
+    IFS='.' read -ra ADDR <<< "`cat $PBS_NODEFILE | head -1`"
+    export MASTER_ADDR=$ADDR".hsn.cm.aurora.alcf.anl.gov"
+elif [[ "$SYSTEM" == "borealis" ]]; then
+    export MASTER_ADDR=$(head -n 1 ${PBS_NODEFILE})
+else
+    echo "Uknown system ${SYSTEM}!"
+    exit 1
+fi
+#export MASTER_ADDR=$(head -n 1 ${PBS_NODEFILE})
 export MASTER_PORT=29500
 
 TODAY=$(date '+%Y-%m-%d')
 TIMENOW=$(date '+%H:%M')
 TIMESTAMP=${TODAY}_${TIMENOW}
-PBS_JOBID_NUM="$( cut -d '.' -f 1 <<< "${PBS_JOBID}" )"
+PBS_JOBNUM="$( cut -d '.' -f 1 <<< "${PBS_JOBID}" )"
 
-RUN_LOG_DIR=./outputs/logs/${SYSTEM}/${LLAMA_CONFIG}/${TODAY}
-RUN_LOG_FILE=${RUN_LOG_DIR}/${LLAMA_CONFIG}_aurora_${NUMNODES}n${NUMPROCS}ppn_${PT_CONFIG}_${PBS_JOBID_NUM}pbs_${TIMESTAMP}_train_log.txt
+LOG_DIR=./outputs/logs/${SYSTEM}/${LLAMA_CONFIG}/${ENV_NAME}/${TODAY}
+LOG_FILE_SUFFIX=${LOG_DIR}/${LLAMA_CONFIG}_${SYSTEM}_${USER}_${NUMNODES}n${NUMPROCS}ppn_${PT_CONFIG}_${PBS_JOBNUM}pbs_${TIMESTAMP}_train
+LOG_FILE=${LOG_FILE_SUFFIX}_log.txt
 
-mkdir -p $RUN_LOG_DIR
+mkdir -p ${LOG_DIR}
+cp ${ENV_REL_PATH} ${LOG_FILE_SUFFIX}_env.sh
+cp ${CONFIG_FILE} ${LOG_FILE_SUFFIX}_config.toml
 
-cp ${ENV_TO_LOAD} ${RUN_LOG_DIR}/${LLAMA_CONFIG}_aurora_${NUMNODES}n${NUMPROCS}ppn_${PT_CONFIG}_${PBS_JOBID_NUM}pbs_${TIMESTAMP}_train_env.sh
-cp ${CONFIG_FILE} ${RUN_LOG_DIR}/${LLAMA_CONFIG}_aurora_${NUMNODES}n${NUMPROCS}ppn_${PT_CONFIG}_${PBS_JOBID_NUM}pbs_${TIMESTAMP}_train_config.toml
-
-echo "[Intel] Running ${LLAMA_CONFIG} on ${SYSTEM^} system using ${NUMNODES} nodes with ${NUMPROCS} processes per node" |& tee $RUN_LOG_FILE
-echo "[Intel] Environment loaded from file $(realpath ./envs/aurora/load_latest_env.sh)" |& tee -a $RUN_LOG_FILE
+echo "[Intel] Running ${LLAMA_CONFIG} on ${SYSTEM^} system using ${NUMNODES} nodes with ${NUMPROCS} processes per node" |& tee ${LOG_FILE}
+echo "[Intel] Environment loaded from file ${ENV_FULL_PATH}" |& tee -a ${LOG_FILE}
 
 mpiexec --envall --pmi=pmix -np ${WORLD_SIZE} -ppn ${NUMPROCS} -l --line-buffer --cpu-bind=${AURORA_CPU_BINDINGS} \
  ./helpers/set_ranks_deps.sh \
  python ../torchtitan/train.py --job.config_file ${CONFIG_FILE} \
- |& tee -a $RUN_LOG_FILE
+ |& tee -a ${LOG_FILE}
