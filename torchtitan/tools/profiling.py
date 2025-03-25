9fde9cfbb7af0278f3bfb286026f21040d01caff
Panagiotis Kourdis <panagiotis.kourdis@intel.com>
2025-03-25 23:43:48 +0000

    [XPU] Enable profiling for XPU devices

--- torchtitan/tools/profiling.py
+++ torchtitan/tools/profiling.py
@@ -57,10 +57,16 @@ def maybe_enable_profiling(config: JobConfig, *, global_step: int = 0):
         assert (
             wait >= 0
         ), "profile_freq must be greater than or equal to warmup + active"
+        if torch.cuda.is_available():
+            gpu_device_profiled = torch.profiler.ProfilerActivity.CUDA
+        elif torch.xpu.is_available():
+            gpu_device_profiled = torch.profiler.ProfilerActivity.XPU
+        else:
+            raise RuntimeError("Profiling supported only for CUDA and XPU devices")
         with torch.profiler.profile(
             activities=[
                 torch.profiler.ProfilerActivity.CPU,
-                torch.profiler.ProfilerActivity.CUDA,
+                gpu_device_profiled,
             ],
             schedule=torch.profiler.schedule(wait=wait, warmup=warmup, active=active),
             on_trace_ready=trace_handler,
