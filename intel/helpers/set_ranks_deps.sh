6a6ed70d6c35de36d7237552ec4acd075dd2c601
Panagiotis Kourdis <panagiotis.kourdis@intel.com>
2025-03-28 06:14:58 +0000

    Fix spacing

--- intel/helpers/set_ranks_deps.sh
+++ intel/helpers/set_ranks_deps.sh
@@ -4,9 +4,9 @@ export LOCAL_RANK=$PALS_LOCAL_RANKID
 export RANK=$PMIX_RANK
 
 if [ $RANK -eq 0 ]; then
-	export TITAN_LOG_LEVEL=INFO
+    export TITAN_LOG_LEVEL=INFO
 else
-	export TITAN_LOG_LEVEL=ERROR
+    export TITAN_LOG_LEVEL=ERROR
 fi
 
 # CCL affinity based on number ranks per node
